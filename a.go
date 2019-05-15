85530537559a44b977a86b1508eff7b9b84f0b98
Cody Soyland <cody@soyland.org>
2019-05-15 08:46:36 -0500

    Add dep gomod-b

--- a.go
+++ a.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	b "github.com/codysoyland/gomod-b"
+)
 
 func main() {
 	fmt.Println("Hello from A")
+	b.Hello()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codysoyland/gomod-a
 
 go 1.12
+
+require github.com/codysoyland/gomod-b v0.0.0-20190515134406-425453df015d // indirect
